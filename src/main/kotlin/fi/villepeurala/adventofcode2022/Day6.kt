d442d3ff4c6d8d552c46db37d4aa88531b9d1229
Ville Peurala <ville.peurala@op.fi>
2022-12-06 07:41:17 +0200

    Clean up after day 6

--- src/main/kotlin/fi/villepeurala/adventofcode2022/Day6.kt
+++ src/main/kotlin/fi/villepeurala/adventofcode2022/Day6.kt
@@ -1,22 +1,24 @@
 package fi.villepeurala.adventofcode2022
 
+import fi.villepeurala.adventofcode2022.Util.inputLines
+
 object Day6 : Day<Int> {
     override val number: Int = 6
 
     override fun part1(inputResourcePath: String): Int {
-        val signal = Util.inputLines(inputResourcePath).first()
+        val signal = inputLines(inputResourcePath).first()
         return findMarker(signal, 4)
     }
 
     override fun part2(inputResourcePath: String): Int {
-        val signal = Util.inputLines(inputResourcePath).first()
+        val signal = inputLines(inputResourcePath).first()
         return findMarker(signal, 14)
     }
 
     fun findMarker(input: String, markerSize: Int): Int {
         val marker = input.windowed(markerSize, 1).withIndex().find {
             // If the window size stays the same after being converted to a set,
-            // it means that all characters in the window are different.
+            // it means that all characters in the window are different -> marker is found.
             it.value.toSet().size == markerSize
         }
         if (marker == null) {
